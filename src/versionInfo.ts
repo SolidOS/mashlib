c14d2842e17b96d39a928a090e9d8c9bbdcfc61a
RubenVerborgh <RubenVerborgh@users.noreply.github.com>
2020-12-07 11:57:28 +0000

    deploy: f88fa6f7763566ed76bc38138cade5163a5be6c3

--- src/versionInfo.ts
+++ src/versionInfo.ts
@@ -1,24 +1,24 @@
 export default {
-  buildTime: '2020-11-30T18:59:24Z',
-  commit: '0d5c063d35fe03edc0d59f5e46bcfa2750280d7a',
+  buildTime: '2020-12-07T11:56:00Z',
+  commit: 'f88fa6f7763566ed76bc38138cade5163a5be6c3',
   npmInfo:
 {
   mashlib: '1.6.7',
-  npm: '7.0.14',
-  node: '15.3.0',
-  v8: '8.6.395.17-node.22',
-  uv: '1.40.0',
-  zlib: '1.2.11',
-  brotli: '1.0.9',
+  npm: '6.14.8',
   ares: '1.16.1',
-  modules: '88',
-  nghttp2: '1.41.0',
-  napi: '7',
-  llhttp: '2.1.3',
-  openssl: '1.1.1g',
+  brotli: '1.0.9',
   cldr: '37.0',
   icu: '67.1',
+  llhttp: '2.1.3',
+  modules: '83',
+  napi: '7',
+  nghttp2: '1.41.0',
+  node: '14.15.1',
+  openssl: '1.1.1g',
   tz: '2020a',
-  unicode: '13.0'
+  unicode: '13.0',
+  uv: '1.40.0',
+  v8: '8.4.371.19-node.17',
+  zlib: '1.2.11'
 }
 }

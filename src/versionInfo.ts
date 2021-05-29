d988550ab95a0cdf998a2cbbfb549cc1e10265c5
angelo-v <angelo-v@users.noreply.github.com>
2021-05-29 21:02:56 +0000

    deploy: 629e6f7cbf4d004b49e4283f57b4ccda2244bb63

--- src/versionInfo.ts
+++ src/versionInfo.ts
@@ -1,24 +1,24 @@
 export default {
-  buildTime: '2021-03-03T11:46:48Z',
-  commit: '3de1e272c1b9280e850f85349255ac68bb2f3ea1',
+  buildTime: '2021-05-29T21:01:35Z',
+  commit: '629e6f7cbf4d004b49e4283f57b4ccda2244bb63',
   npmInfo:
 {
   mashlib: '1.6.9',
-  npm: '6.14.11',
-  ares: '1.16.1',
+  npm: '6.14.13',
+  ares: '1.17.1',
   brotli: '1.0.9',
-  cldr: '37.0',
-  icu: '67.1',
+  cldr: '38.1',
+  icu: '68.2',
   llhttp: '2.1.3',
   modules: '83',
-  napi: '7',
-  nghttp2: '1.41.0',
-  node: '14.15.5',
-  openssl: '1.1.1i',
-  tz: '2020a',
+  napi: '8',
+  nghttp2: '1.42.0',
+  node: '14.17.0',
+  openssl: '1.1.1k',
+  tz: '2020d',
   unicode: '13.0',
-  uv: '1.40.0',
-  v8: '8.4.371.19-node.18',
+  uv: '1.41.0',
+  v8: '8.4.371.23-node.63',
   zlib: '1.2.11'
 }
 }

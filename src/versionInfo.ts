4e624bfae5930c4d2ec9f249462cfdf8e1df2afc
bourgeoa <bourgeoa@users.noreply.github.com>
2021-11-06 18:30:39 +0000

    deploy: 2021c2fcea5ee9269ce337e6849ac4efb6ff496d

--- src/versionInfo.ts
+++ src/versionInfo.ts
@@ -1,9 +1,9 @@
 export default {
-  buildTime: '2021-11-05T15:27:53Z',
-  commit: '7861c5036714fd15817ec5c5816c2e6a34d31f2c',
+  buildTime: '2021-11-06T18:28:23Z',
+  commit: '2021c2fcea5ee9269ce337e6849ac4efb6ff496d',
   npmInfo:
 {
-  mashlib: '1.7.9',
+  mashlib: '1.7.10',
   npm: '6.14.15',
   ares: '1.17.2',
   brotli: '1.0.9',

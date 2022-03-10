3225c195be425a05ef6525820882545a7dcd5506
theRealImy <theRealImy@users.noreply.github.com>
2022-03-10 09:35:40 +0000

    deploy: 9a963b4325fb09bb1960f1e49fd286ca37ae34d0

--- src/versionInfo.ts
+++ src/versionInfo.ts
@@ -1,24 +1,26 @@
 export default {
-  buildTime: '2022-03-10T08:44:42Z',
-  commit: '46ac1ee9fedd86d876ffd9c7b141a9e16e4787d1',
+  buildTime: '2022-03-10T09:34:20Z',
+  commit: '9a963b4325fb09bb1960f1e49fd286ca37ae34d0',
   npmInfo:
 {
   mashlib: '1.7.22',
-  npm: '6.14.16',
-  ares: '1.18.1',
+  npm: '8.3.1',
+  node: '16.14.0',
+  v8: '9.4.146.24-node.20',
+  uv: '1.43.0',
+  zlib: '1.2.11',
   brotli: '1.0.9',
+  ares: '1.18.1',
+  modules: '93',
+  nghttp2: '1.45.1',
+  napi: '8',
+  llhttp: '6.0.4',
+  openssl: '1.1.1m+quic',
   cldr: '40.0',
   icu: '70.1',
-  llhttp: '2.1.4',
-  modules: '83',
-  napi: '8',
-  nghttp2: '1.42.0',
-  node: '14.19.0',
-  openssl: '1.1.1m',
   tz: '2021a3',
   unicode: '14.0',
-  uv: '1.42.0',
-  v8: '8.4.371.23-node.85',
-  zlib: '1.2.11'
+  ngtcp2: '0.1.0-DEV',
+  nghttp3: '0.1.0-DEV'
 }
 }

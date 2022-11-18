031d86bdcebc57cf8fb5be4c9380ff90e0d6a98d
bourgeoa <bourgeoa@users.noreply.github.com>
2022-11-18 15:49:35 +0000

    deploy: 32269e6fe51473629b3d56e2a07d1fc0459a0259

--- src/versionInfo.ts
+++ src/versionInfo.ts
@@ -1,11 +1,11 @@
 export default {
-buildTime: "2022-09-01T21:01:27Z",
-commit: "f1102962164341bc9918128752dd1ce7df3308e9",
+buildTime: "2022-11-18T15:47:40Z",
+commit: "32269e6fe51473629b3d56e2a07d1fc0459a0259",
 npmInfo:
 {
-  mashlib: '1.8.4',
-  npm: '8.18.0',
-  node: '14.20.0',
+  mashlib: '1.8.5',
+  npm: '8.19.3',
+  node: '14.20.1',
   v8: '8.4.371.23-node.87',
   uv: '1.42.0',
   zlib: '1.2.11',
@@ -14,7 +14,7 @@ npmInfo:
   modules: '83',
   nghttp2: '1.42.0',
   napi: '8',
-  llhttp: '2.1.5',
+  llhttp: '2.1.6',
   openssl: '1.1.1q',
   cldr: '40.0',
   icu: '70.1',

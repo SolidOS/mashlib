7cf7da1263dd3556844f96066d0ea356c5f3eebc
SharonStrats <SharonStrats@users.noreply.github.com>
2023-04-08 21:24:39 +0000

    deploy: ce3e55a4f10e30c503a1c854b7e37b9a02ea0d82

--- src/versionInfo.ts
+++ src/versionInfo.ts
@@ -1,16 +1,16 @@
 export default {
-buildTime: "2023-03-17T15:32:39Z",
-commit: "5e5bbf6e7b264c577e90478def4386bb64d81d12",
+buildTime: "2023-04-08T21:23:10Z",
+commit: "ce3e55a4f10e30c503a1c854b7e37b9a02ea0d82",
 npmInfo:
 {
   mashlib: '1.8.8',
   npm: '8.19.4',
-  node: '16.19.1',
-  v8: '9.4.146.26-node.24',
+  node: '16.20.0',
+  v8: '9.4.146.26-node.26',
   uv: '1.43.0',
   zlib: '1.2.11',
   brotli: '1.0.9',
-  ares: '1.18.1',
+  ares: '1.19.0',
   modules: '93',
   nghttp2: '1.47.0',
   napi: '8',

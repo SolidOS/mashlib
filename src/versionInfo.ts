7b5c9f956b7640c0fe1a2c2712b543e6a403d89d
bourgeoa <bourgeoa@users.noreply.github.com>
2025-02-14 08:09:52 +0000

    deploy: 656fb3b81e7b9bfed59c3e9efd2c59947789b92c

--- src/versionInfo.ts
+++ src/versionInfo.ts
@@ -1,9 +1,9 @@
 export default {
-buildTime: "2025-02-05T10:50:50Z",
-commit: "d7cb1847b5cf4ddf8be2d48a91e1a2fe6f5c7e87",
+buildTime: "2025-02-14T08:08:47Z",
+commit: "656fb3b81e7b9bfed59c3e9efd2c59947789b92c",
 npmInfo:
 {
-  mashlib: '1.10.3',
+  mashlib: '1.10.4',
   npm: '8.19.4',
   node: '18.20.6',
   acorn: '8.13.0',

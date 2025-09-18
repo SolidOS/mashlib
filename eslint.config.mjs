9c8757e741dc29c37fccc70ec2ba65b883b0aa42
timea-solid <4144203+timea-solid@users.noreply.github.com>
2025-09-18 22:15:17 +0200

    new lint, tsc and webpack config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,60 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
+import tseslintPlugin from '@typescript-eslint/eslint-plugin'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
 
-export default [{
-    ignores: ["**/dist"],
-}, {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
+export default [
+    {
+        ignores: [
+            'node_modules/**',
+            'dist/**',
+        ],
     },
+    {
+        files: ['src/**/*.js', 'src/**/*.cjs', 'src/**/*.mjs'],
 
-    languageOptions: {
+        languageOptions: {
         globals: {
             ...globals.browser,
             ...globals.node,
-            Atomics: "readonly",
-            SharedArrayBuffer: "readonly",
+            Atomics: 'readonly',
+            SharedArrayBuffer: 'readonly',
+        }
         },
-
-        parser: tsParser,
+        rules: {
+        semi: ['error', 'never'],
+        quotes: ['error', 'single'],
+        'no-console': 'error',
+        'no-unused-vars': 'error',
+        'no-undef': 'error'
+        },
     },
-    files: ["src/**/*.ts"],
-    rules: {
-        "no-unused-vars": ["warn", {
-            argsIgnorePattern: "^_",
-            varsIgnorePattern: "^_",
-        }],
-
-        "@typescript-eslint/no-unused-vars": ["warn", {
-            argsIgnorePattern: "^_",
-            varsIgnorePattern: "^_",
-        }],
-    },
-}];
+    {
+        files: ["src/**/*.ts"],
+        plugins: {
+            "@typescript-eslint": tseslintPlugin,
+        },
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                Atomics: "readonly",
+                SharedArrayBuffer: "readonly",
+            },
+            parser: tsParser,
+            parserOptions: {
+                project: ['./tsconfig.json']
+            },
+        },
+        
+        rules: {
+            semi: ['error', 'never'],
+            quotes: ['error', 'single'],
+            'no-console': 'warn',
+            'no-unused-vars': 'off',
+            "@typescript-eslint/no-unused-vars": ["warn", {
+                argsIgnorePattern: "^_",
+                varsIgnorePattern: "^_",
+            }],
+        },
+    }
+]

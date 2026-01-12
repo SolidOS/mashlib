0e6425b6c38d93d053be2b89bff4dfee9c0fce95
timea-solid <timea-solid@users.noreply.github.com>
2026-01-12 11:54:27 +0000

    deploy: 9d55b0202a41066ee9260ab5fca7421f9a23aced

--- src/versionInfo.ts
+++ src/versionInfo.ts
@@ -1,10 +1,10 @@
 export default {
-  buildTime: '2025-11-29T22:26:50Z',
-  commit: '231ce02c54ab154101014bc68c845d440795d3e7',
+  buildTime: '2026-01-12T11:53:28Z',
+  commit: '9d55b0202a41066ee9260ab5fca7421f9a23aced',
   npmInfo: {
     'mashlib': '2.0.0',
     'npm': '10.8.2',
-    'node': '20.19.5',
+    'node': '20.19.6',
     'acorn': '8.15.0',
     'ada': '2.9.2',
     'ares': '1.34.5',
@@ -16,14 +16,14 @@ export default {
     'modules': '115',
     'napi': '9',
     'nghttp2': '1.61.0',
-    'openssl': '3.0.16',
+    'openssl': '3.0.17',
     'simdutf': '6.4.2',
     'tz': '2025b',
-    'undici': '6.21.2',
+    'undici': '6.22.0',
     'unicode': '16.0',
     'uv': '1.46.0',
-    'uvwasi': '0.0.21',
-    'v8': '11.3.244.8-node.30',
+    'uvwasi': '0.0.23',
+    'v8': '11.3.244.8-node.33',
     'zlib': '1.3.1-470d3a2',
   }
 }
